731812ce9fac7118380fb1992a294a3a03a1b2f2
Lemein <lemeinannette@gmail.com>
2023-11-04 08:53:21 +0300

    completed lab

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,24 @@
+// Actors.js
 import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      {actors.map((actor) => (
+        <div key={actor.name}>
+          <h2>Name: {actor.name}</h2>
+          <p>Movies:</p>
+          <ul>
+            {actor.movies.map((movie, index) => (
+              <li key={index}>{movie}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Actors;
